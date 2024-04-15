45f7024263a855c55813ed70bc3c319d0f7e635d
pehlivan2022 <99028696+pehlivan2022@users.noreply.github.com>
2024-04-15 05:25:54 +0200

    TOUR VIRTUAL

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-dji_0049-copia",
-      "name": "DJI_0049 copia",
+      "id": "0-ristorante",
+      "name": "RISTORANTE",
       "levels": [
         {
           "tileSize": 256,
@@ -24,42 +24,207 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.5008214646002784,
-        "pitch": 0.043411547394176964,
+        "yaw": 1.6691527149300471,
+        "pitch": 0.07113269222188023,
         "fov": 1.4300641596566168
       },
       "linkHotspots": [
         {
-          "yaw": 1.7458768819047688,
-          "pitch": 0.054155487664358404,
+          "yaw": -1.4470097783681162,
+          "pitch": 0.17188487010409048,
           "rotation": 0,
-          "target": "2-dji_0031"
+          "target": "1-terrazza-colazioni-1-"
         },
         {
-          "yaw": -1.386480194194041,
-          "pitch": 0.005302201052407085,
-          "rotation": 0,
-          "target": "1-dji-sala-colazioni-1"
+          "yaw": -1.132202568233673,
+          "pitch": 0.042511660346495006,
+          "rotation": 0.7853981633974483,
+          "target": "13-goletta-ricevimento"
         },
         {
-          "yaw": -2.8523484414627696,
-          "pitch": 0.055395406128083025,
+          "yaw": -1.8397393804098847,
+          "pitch": 0.053814822652851646,
+          "rotation": 5.497787143782138,
+          "target": "11-reading-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-terrazza-colazioni-1-",
+      "name": "TERRAZZA COLAZIONi 1 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.24507248043840235,
+        "pitch": 0.18866537145710893,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8206766911654393,
+          "pitch": 0.16836040981760547,
+          "rotation": 6.283185307179586,
+          "target": "2-terrazza-colazioni-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-terrazza-colazioni-2",
+      "name": "TERRAZZA COLAZIONI 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7930545375102067,
+          "pitch": 0.024568550990540672,
           "rotation": 0,
-          "target": "3-dji_0036"
+          "target": "1-terrazza-colazioni-1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4002-luxury-suite-sleeping",
+      "name": "4002 LUXURY SUITE SLEEPING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.120279663761327,
+          "pitch": -0.034436720619394734,
+          "rotation": 0,
+          "target": "4-4002-luxury-suite-living"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 1.1449952201008635,
+          "yaw": 0,
           "pitch": 0,
-          "title": "Title",
+          "title": "Probably the best view in town !",
           "text": "Text"
         }
       ]
     },
     {
-      "id": "1-dji-sala-colazioni-1",
-      "name": "DJI sala colazioni (1)",
+      "id": "4-4002-luxury-suite-living",
+      "name": "4002 LUXURY SUITE LIVING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.7422381092035018,
+        "pitch": 0.22500380976924284,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06741736560992351,
+          "pitch": 0.1912961171452494,
+          "rotation": 0,
+          "target": "3-4002-luxury-suite-sleeping"
+        },
+        {
+          "yaw": 3.009734283617263,
+          "pitch": 0.061421800370251134,
+          "rotation": 0,
+          "target": "6-deluxe-suite_1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.168826529690975,
+          "pitch": 0.3420005326437625,
+          "title": "LIVING ROOM WITH KITCHEN",
+          "text": "Microwave - Coffee machine - etc"
+        }
+      ]
+    },
+    {
+      "id": "5-deluxe-suite-2",
+      "name": "DELUXE SUITE 2",
       "levels": [
         {
           "tileSize": 256,
@@ -89,8 +254,255 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "2-dji_0031",
-      "name": "DJI_0031",
+      "id": "6-deluxe-suite_1",
+      "name": "DELUXE SUITE_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.050613303166222,
+          "pitch": 0.06744660436995176,
+          "rotation": 0,
+          "target": "4-4002-luxury-suite-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-suite-sleeping",
+      "name": "SUITE sleeping",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.108145562920164,
+          "pitch": 0.19330311764925412,
+          "rotation": 0,
+          "target": "8-suite-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-suite-living",
+      "name": "SUITE living",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.8319673348537222,
+        "pitch": 0.059889739711859846,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0079869518748534,
+          "pitch": 0.0825275453178751,
+          "rotation": 0,
+          "target": "7-suite-sleeping"
+        },
+        {
+          "yaw": -0.1175861043460742,
+          "pitch": 0.17577992253824704,
+          "rotation": 0,
+          "target": "10-junior-suite"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-junior-suite-2",
+      "name": "JUNIOR SUITE 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.268062615500698,
+          "pitch": 0.0018866281789637895,
+          "rotation": 0,
+          "target": "1-terrazza-colazioni-1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-junior-suite",
+      "name": "JUNIOR SUITE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08941635443390439,
+        "pitch": 0.09244478076936247,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2505646015181773,
+          "pitch": 0.06764651795989174,
+          "rotation": 0,
+          "target": "9-junior-suite-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-reading-room",
+      "name": "READING ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.125616109598502,
+        "pitch": 0.1544812037108656,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2928604570995699,
+          "pitch": 0.05136513505825491,
+          "rotation": 0,
+          "target": "0-ristorante"
+        },
+        {
+          "yaw": 0.3870009798046432,
+          "pitch": 0.11777437079062736,
+          "rotation": 0,
+          "target": "10-junior-suite"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1423184440983496,
+          "pitch": 0.13777886921102223,
+          "title": "READING ROOM",
+          "text": "Hotel Lobby<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "12-goletta-esterno_1",
+      "name": "goletta esterno_1",
       "levels": [
         {
           "tileSize": 256,
@@ -120,8 +532,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "3-dji_0036",
-      "name": "DJI_0036",
+      "id": "13-goletta-ricevimento",
+      "name": "GOLETTA RICEVIMENTO",
       "levels": [
         {
           "tileSize": 256,
